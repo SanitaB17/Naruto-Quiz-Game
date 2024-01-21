2d57e68d982a420fab9746b5ed327472e3f6c4fa
Sanita Bruvere <sanita.bruvere90@gmail.com>
2024-01-21 06:31:20 +0000

    Add style,img and function to button

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+console.log("Hello");
+
+//start ===  code taken from w3 schools.com https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  // End of code
