47ef7ad7ebc1689efd88fef60ea58491fec20ee1
Sanita Bruvere <sanita.bruvere90@gmail.com>
2024-01-28 22:57:40 +0000

    Change style, add function to rules btn and start quiz btn

---
+++ assets/js/data.js
@@ -0,0 +1,59 @@
+// Array for questions
+
+const questions = [
+    {
+      number: 1,
+      question: "What is Naruto Uzumaki's signature jutsu?",
+      answer: "Shadow Clone Jutsu",
+      options: [
+        "Shadow Clone Jutsu",
+        "Rasengan",
+        "Rasenshuriken",
+        "Tailed Beast Mode",
+      ]
+    },
+    {
+      number: 2,
+      question: "Which tailed beast is sealed within Naruto?",
+      answer: "Kurama",
+      options: [
+        "Shukaku",
+        "Matatabi",
+        "Son Goku",
+        "Kurama",
+      ]
+    },
+    {
+      number: 3,
+      question: "What is the name of Naruto's sensei during his early training days?",
+      answer: "Iruka Umino",
+      options: [
+        "Tsunade",
+        "Iruka",
+        "Yamato",
+        "Kakashi",
+      ]
+    },
+    {
+      number: 4,
+      question: "What is Naruto's favorite food?",
+      answer: "Ramen",
+      options: [
+        "Chips",
+        "Fish",
+        "Ramen",
+        "Rice Balls",
+      ]
+    },
+    {
+      number: 5,
+      question: "Who is Naruto's closest friend?",
+      answer: "Sasuke Uchiha",
+      options: [
+        "Sakura Haruno",
+        "Hinata Hyuga",
+        "Sasuke Uchiha",
+        "Shikamaru Nara",
+      ]
+    }
+  ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
-console.log("Hello");
-
-//start  code taken from w3 schools.com https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
-  // End of code
+const rulesDisplay = document.getElementById("rules-display");
+const myDiv = document.getElementById("myDiv");
+const startQuizBtn = document.getElementById("start-quiz-btn");
+const homeMain = document.getElementById("home");
+const questionSection = document.getElementById("question-section");
+const scoreSection = document.getElementById("score-section");
+//shows game rules 
+//start of code taken and modified from w3 schools.com https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
+rulesDisplay.onclick = ()=>{
+  var x = document.getElementById("myDIV");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+}
+// End of taken code
+// Hide sections when clicked on start quiz button and show questions section only
+startQuizBtn.addEventListener("click", () => {
+  homeMain.style.display = "none";
+  scoreSection.style.visibility = "hidden";
+  questionSection.style.visibility = "visible";
+});
