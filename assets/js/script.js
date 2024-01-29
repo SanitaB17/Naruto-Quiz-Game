e0d92e706f5b57c40fd3348fc83bce3089c353ff
Sanita Bruvere <sanita.bruvere90@gmail.com>
2024-01-29 01:13:04 +0000

    Add Next button to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 const rulesDisplay = document.getElementById("rules-display");
-const myDiv = document.getElementById("myDiv");
+//const myDiv = document.getElementById("myDiv");
+const header = document.getElementById("header");
 const startQuizBtn = document.getElementById("start-quiz-btn");
 const homeMain = document.getElementById("home");
 const questionSection = document.getElementById("question-section");
 const scoreSection = document.getElementById("score-section");
-//shows game rules 
-//start of code taken and modified from w3 schools.com https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
+const optionButtons = document.getElementById("option-buttons");
+const playAgainButton = document.getElementById("play-again-btn");
+const backHomeButton = document.getElementById("back-home-btn");
+const questionText = document.getElementById("question");
+
+// shows game rules 
+// start of code taken and modified from w3 schools.com https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show
 rulesDisplay.onclick = ()=>{
   var x = document.getElementById("myDIV");
       if (x.style.display === "none") {
@@ -17,6 +23,7 @@ rulesDisplay.onclick = ()=>{
 // End of taken code
 // Hide sections when clicked on start quiz button and show questions section only
 startQuizBtn.addEventListener("click", () => {
+  header.style.display = "none"
   homeMain.style.display = "none";
   scoreSection.style.visibility = "hidden";
   questionSection.style.visibility = "visible";
